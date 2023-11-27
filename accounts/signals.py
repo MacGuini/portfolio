cb8358eac02b305d062c6d3ecc8e25de9626990f
macguini <bhatz829@gmail.com>
2023-11-26 22:13:26 -0500

    possible recaptcha fix

--- accounts/signals.py
+++ accounts/signals.py
@@ -1,6 +1,5 @@
 from django.db.models.signals import post_delete, post_save
 from django.dispatch import receiver
-from django.core.mail import send_mail
 
 from django.contrib.auth.models import User
 from .models import Profile
@@ -11,18 +10,18 @@ def generate_password(length):
     password = ''.join(secrets.choice(alphabet) for i in range(length))
     return password
 
-@receiver(post_save, sender=User)
-def createProfile(sender, instance, created, **kwargs):
-	if created:
-		user = instance
-		profile = Profile.objects.create(
-			user = user,
-			username = user.username,
-			email = user.email,
-			fname = user.first_name,
-			lname = user.last_name,
-		)
-		profile.save()
+# @receiver(post_save, sender=User)
+# def createProfile(sender, instance, created, **kwargs):
+# 	if created:
+# 		user = instance
+# 		profile = Profile.objects.create(
+# 			user = user,
+# 			username = user.username,
+# 			email = user.email,
+# 			fname = user.first_name,
+# 			lname = user.last_name,
+# 		)
+# 		profile.save()
 
 		
 @receiver(post_save, sender=Profile)
@@ -77,14 +76,6 @@ def updateProfile(sender, instance, created, **kwargs):
 		user.last_name = profile.lname
 		user.email = profile.email
 		user.save()
-
-		send_mail(
-			"Your profile was updated!",
-			"This message is being sent to you to let you know that your profile at sublimeimprovements.com has been successfully updated.",
-			"noreply@sublimeimprovements.com",
-			[profile.email],
-			fail_silently=False,
-		)
 
 @receiver(post_delete, sender=Profile)
 def deleteUser(sender, instance, **kwargs):
